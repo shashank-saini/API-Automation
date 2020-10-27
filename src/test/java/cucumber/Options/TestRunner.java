e82816c4e0e9c7514b1e4529ade1d75e30fdac30
Shashank Saini <72013734+shashank-saini@users.noreply.github.com>
2020-10-27 16:11:27 +0530

    Update TestRunner.java
    commit

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -1,14 +1,13 @@
 package cucumber.Options;
 
-package cucumber.Options;
-
 import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
 import cucumber.api.CucumberOptions.*;
 import cucumber.api.junit.*;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features="/var/jenkins_home/workspace/API-Automation/src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepDefinations"})
 public class TestRunner {
-//tags= {"@DeletePlace"}  compile test verify
+
 }
